fa76b6467a209342213226a43e5db4440cdc4ce5
820370-kleeq <820370@manadulted.ac.uk>
2025-11-05 10:32:24 +0000

    Fixed task 4

--- Ex2_review.py
+++ Ex2_review.py
@@ -147,7 +147,7 @@ print("-------------------------------------------\n"
 #
 # Write your code below:
 num = int(input("Enter a number: "))
-for i in range (1, 10):
+for i in range (1, 11):
     print (f"{num} x {i} = {num * i}")
 
 # -------------------------------------------
